68c1a570a2dee2f395740aefe1b36198f54abeaf
Germain RULLIER <grullier@free.fr>
2021-11-28 17:59:01 +0100

    Color

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void process_input(GLFWwindow* window);
 
 const char* TITLE = "Babyfoot";
 const unsigned int SCREEN_WIDTH = 960;
@@ -31,9 +32,37 @@ int main(int argc, char const *argv[])
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    // glad init : loads all OpenGL functions pointers
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // render loop
+    while (!glfwWindowShouldClose(window)) {
+        // input
+        process_input(window);
+
+        // render
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // glfw: swap buffers and poll IO events (keys, mouse, ...)
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
+}
+
+void process_input(GLFWwindow* window) {
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+        
 }
