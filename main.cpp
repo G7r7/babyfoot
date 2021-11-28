6bb3b7c1dbe443e8e655e6ab8d00f3cab05a7ccb
Germain RULLIER <grullier@free.fr>
2021-11-28 15:49:51 +0100

    Cmake init

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int init = glfwInit();
+    std::cout << "J'ai init ! Ca a fait ca : " << init;
     return 0;
 }
