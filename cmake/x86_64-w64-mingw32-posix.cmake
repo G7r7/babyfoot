ad5fdfb1f7dbe61ca826f2bdd8cd7758a508e470
Germain RULLIER <grullier@free.fr>
2022-08-11 09:54:22 +0000

    Modern CMake

---
+++ cmake/x86_64-w64-mingw32-posix.cmake
@@ -0,0 +1,8 @@
+# the name of the target operating system
+set(CMAKE_SYSTEM_NAME Windows)
+
+# which compilers to use for C and C++
+set(CMAKE_C_COMPILER   x86_64-w64-mingw32-gcc-posix)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++-posix)
+
+set(CMAKE_CXX_STANDARD 11)
--- cmake/x86_64-w64-mingw32.cmake
+++
@@ -1,6 +0,0 @@
-# the name of the target operating system
-set(CMAKE_SYSTEM_NAME Windows)
-
-# which compilers to use for C and C++
-set(CMAKE_C_COMPILER   x86_64-w64-mingw32-gcc)
-set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
